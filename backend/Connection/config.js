19bfb58e18f5449c840d79819978c26f0fc6f894
Ajay6599 <ajayprajapati6599@gmail.com>
2025-07-12 12:37:38 +0530

    Add LMS Project

--- backend/Connection/config.js
+++ backend/Connection/config.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-let connection = mongoose.connect('mongodb+srv://ajayprajapati:pTrsc8VxRjjPmdSa@cluster0.kowkh.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0');
+// let connection = mongoose.connect('mongodb+srv://ajayprajapati:pTrsc8VxRjjPmdSa@cluster0.kowkh.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0');
+
+let connection = mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { connection };
